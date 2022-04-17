d3ebac382aa06f3e41ae9bb891ad5f065263a784
EmersuCC <62714085+EmersuCC@users.noreply.github.com>
2022-04-17 02:13:11 -0300

    create table de usuários

---
+++ table.py
@@ -0,0 +1,21 @@
+
+#NÃO EXECUTE ESSE CÓDIGO
+
+"""
+import sqlite3
+conn = sqlite3.connect('user.db')
+cursor = conn.cursor()
+def create_table():
+    cursor.execute('''
+        CREATE TABLE users(
+            username VARCHAR(12) NOT NULL,
+            password VARCHAR(20) NOT NULL
+        );
+
+    ''')
+    print('Tabela criada')
+
+
+create_table()
+
+"""
