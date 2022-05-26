33de05def48431c0d1c2f70bf32c25c16ba00b4f
Luiz Martins <luiz00martins@pop-os.localdomain>
2022-05-25 21:36:47 -0300

    feat: exhibiting users by birthday in decreasing order

--- Tests.py
+++ Tests.py
@@ -1,15 +1,37 @@
 from os import wait
 import sqlite3
-from Packages import Users, Institute, Turma, Treeset, Date
+from Packages import Users, Institute, Turma
 from Packages.Date import Date
+from Packages.Treeset import TreeSet
 
-# Testing database.
 def init_db(conn: sqlite3.Connection):
     with open('db_definition.sql', 'r') as f:
         contents = f.read()
         cur = conn.cursor()
         cur.executescript(contents)
     
+class UserComparer():
+    def __init__(self, user):
+        self.user = user
+
+    def __str__(self):
+        return str(self.user)
+
+    def __lt__(self, other):
+        return self.user.birthday < other.user.birthday
+    def __eq__(self, other):
+        return self.user.birthday == other.user.birthday
+    def __gt__(self, other):
+        return self.user.birthday > other.user.birthday
+    def __ne__(self, other):
+        return self.user.birthday != other.user.birthday
+    def __le__(self, other):
+        return self.user.birthday <= other.user.birthday
+    def __ge__(self, other):
+        return self.user.birthday >= other.user.birthday
+
+def users_by_birthday_in_decreasing_order(users):
+    return TreeSet([UserComparer(u) for u in users])
 
 conn = sqlite3.connect('user.db')
 with conn:
@@ -26,18 +48,21 @@ with conn:
 
     # Tests
 
-    user1 = Users.User('addtest', 'addpasstest22', Date(1,5,2010))
+    user1 = Users.User('addtest', 'addpasstest22', Date(1,5,2015))
     user2 = Users.User('addtesta', 'addpasstest22', Date(1,6,2010))
-    user3 = Users.User('add1testa', 'addpasstest22', Date(2,6,2010))
+    user3 = Users.User('addtestb', 'addpasstest22', Date(30,10,1999))
+
+    userError0 = Users.User('add1testa', 'addpasstest22', Date(2,6,1995))
 
     validation = Users.UserDefaultValidation()
 
     user_repo.add_user(user1, validation)
+    user_repo.add_user(user3, validation)
     user2_id = user_repo.add_user(user2, validation)
 
-    print('Print all: ')
+    print('Print all by birthday: ')
     users = user_repo.get_all()
-    for user in users:
+    for user in users_by_birthday_in_decreasing_order(users):
         print('\t', user)
 
     print('Print by id, username: ')
@@ -51,19 +76,19 @@ with conn:
     print('Should be None: ', user_repo.get_user_by_username('ohno')) # Returns 'None'
 
     try:
-        user_repo.add_user(user3, validation)
+        user_repo.add_user(userError0, validation)
     except Users.UserValidationException as err:
         print(err)
 
-    user4 = Users.User('usersab', 'passuser32', Date(1, 10, 2001))
-    user_repo.add_user(user4, validation)
+    userError1 = Users.User('usersab', 'passuserError02', Date(1, 10, 2001))
+    user_repo.add_user(userError1, validation)
 
     print('Print all: ')
     users = user_repo.get_all()
     for user in users:
         print('\t', user)
     
-    user_repo.remove_user_by_id(user4.id)
+    user_repo.remove_user_by_id(userError1.id)
 
     print('Print all: ')
     users = user_repo.get_all()
@@ -96,6 +121,6 @@ with conn:
 
 
 # Testing dates and treeset.
-ts = Treeset.TreeSet([Date(1,5,2010),Date(1,5,2010),Date(2,5,2010),Date(2,5,2010),Date(1,5,1999)])
+ts = TreeSet([Date(1,5,2010),Date(1,5,2010),Date(2,5,2010),Date(2,5,2010),Date(1,5,1999)])
 print(ts)
 
